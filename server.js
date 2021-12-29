a00d41a40d76519df671f7b0aaff9b08e39cfe18
Vikram Negi <viknegi0@gmail.com>
2021-12-29 10:04:56 +0530

    basic server

--- server.js
+++ server.js
@@ -2,3 +2,10 @@
 
 const http = require("http");
 const WebSocketServer = require("websocket").server
+
+// raw http server, this will help us create a TCP for websocket
+const httpServer = http.createServer((req, res) => {
+  console.log("Server received a request!");
+});
+
+// httpServer.listen(8080, ())
